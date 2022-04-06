9c317208966bd7fbbda74619a472d3a4c275cb24
Sarah Lopez <slopez323@gmail.com>
2022-04-06 18:56:55 -0400

    show comments

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let topIDs = [];
 let count = 1;
+let dataComments = [];
 
 async function getTopIDs() {
     let httpResponse = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
@@ -9,23 +10,48 @@ async function getTopIDs() {
 
 getTopIDs();
 
-async function getDetails(id){
-    let details = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
-    details = await details.json();
-    console.log(details);
-    createList(details);
-};
-
-function createList(details){
-    let itemNum = `<td class="itemNum">${count}. </td>`
-    let newItemTitle = `<td class="titleParent"><a class="itemTitle" href="${details.url}">${details.title}</a></td>`
-    let newItemDesc = `<td class="itemDesc">${details.score} points by ${details.by} | ${details.descendants} comments</td>`
-    $('table').append(`<tr>${itemNum}${newItemTitle}</tr><tr><td></td>${newItemDesc}</tr>`)
-    count++;
-};
-
-function top100(){
-    for (let i=0; i < 100; i++){
+function top100() {
+    for (let i = 0; i < 100; i++) {
         getDetails(topIDs[i]);
     };
 };
+
+$('.storyList').on('click', '.showComment', (e) => {
+    const parentdiv = $(e.target).parent().parent();
+    const storyID = $(e.target).attr("data-story-id");
+
+    if ($(e.target).parent().next().length == 0) {
+        showComments(parentdiv, storyID);
+    } else {
+        $(e.target).parent().next().toggle();
+    };
+});
+
+async function getDetails(id) {
+    let details = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+    details = await details.json();
+    dataComments.push({ id, comments: details.kids });
+    createList(details);
+};
+
+function createList(details) {
+    let itemNum = `<span class="itemNum">${count}. </span>`
+    let newItemTitle = `<span class="titleParent"><a class="itemTitle" href="${details.url}">${details.title}</a></span>`
+    let newItemDesc = `<p class="itemDesc">${details.score} points by ${details.by} | <span class="showComment" data-story-id="${details.id}">${details.descendants} comments</span></p>`
+    $('.storyList').append(`<div class="storyItem">${itemNum}${newItemTitle}${newItemDesc}</div>`)
+    count++;
+};
+
+
+async function showComments(parentdiv, storyID) {
+    const commentID = dataComments.find(x => x.id == storyID).comments
+    $(parentdiv).append(`<div class="commentList"></div>`);
+    if (commentID.length > 0) {
+        for (let i = 0; i < commentID.length; i++) {
+            let comments = await fetch(`https://hacker-news.firebaseio.com/v0/item/${commentID[i]}.json?print=pretty`);
+            comments = await comments.json();
+            $(parentdiv).find('.commentList').append(`<p class="commentBy">${comments.by}</p><div class="commentText">${comments.text}</div>`);
+        };
+    };
+};
+
